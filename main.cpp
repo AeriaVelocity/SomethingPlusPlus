ec0eb675e7ca248df08c00cb436f82a4f9fea514
Astrid Starii Kazmi <sonicspeed848@gmail.com>
2021-04-29 19:27:21 +0100

    Removed login requirement

--- main.cpp
+++ main.cpp
@@ -1,110 +1,124 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <cctype>
 #include <string>
-#include "progdefs.h"
+#ifndef _WIN32
+#include <unistd.h>
+#include <sys/types.h> 
+#endif
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc > 2)
 	{
-		cout << NoArgsErr;
-        return 1;
-	}
-	else if (argc < 3)
-	{
-		cout << NoUsernameErr;
-        return 1;
+		cout << "We don't accept " << argv[2] << "here. Sorry." << endl;
+		return EXIT_FAILURE;
 	}
 	else
 	{
-	    string Username = "";
-		Username = argv[2];
-		std::string PasswordField;
-		if (strcmp(argv[1], "login") == 0)
+		string Username;
+	    if (argc > 1)
 		{
-			cout << "Please enter your password: ";
-			getline(cin, PasswordField);
-			if (PasswordField == CorrectPassword)
-			{
-				cout << "Welcome, " << Username << "! Creating key file..." << endl;
-				FILE *KeyFilePointer;
-				KeyFilePointer = fopen("keyfile.kfl", "w");
-				fclose(KeyFilePointer);
-				cout << "Enter a command!\n";
-				string Command = "";
-                int LoginFun = 0;
-				while (Command != "exit")
-                {
-                    cout << Username << " > ";
-                    getline(cin, Command);
-                    if (Command == "cd")
-                    {
-                        cout << "There is no CD in the computer.\n" <<
-                        "Not only that, but there are no CD drivers in this program.\n" << endl;
-                    } 
-                    else if (Command == "ls")
-                    {
-                        cout << "There are no lemons to salvage.\n" <<
-                        "Please install a basket in your computer's hard drive and fill it with lemons.\n" << endl;
-                    } 
-                    else if (Command == "dir")
-                    {
-                        cout << "Unable to drive in the road.\n" <<
-                        "The driver CAR.DLL was not found.\n" << endl;
-                    } 
-                    else if (Command == "login")
-                    {
-                        LoginFun++;
-                        if (LoginFun > 0 and LoginFun < 2)
-                        {
-                            cout << "in\n" << endl;
-                        }
-                        else if (LoginFun > 1 and LoginFun < 3)
-                        {
-                            cout << "What? I already DID log in.\n" << endl;
-                        }
-                        else if (LoginFun > 2 and LoginFun < 4)
-                        {
-                            cout << "As in... literally logged 'in' to the console.\n" << 
-                            "What did you expect?\n" << endl;
-                        }
-                        else
-                        {
-                            cout << "in\n" << endl;
-                        }
-                    } 
-                    else if (Command == "exit")
-                    {
-                        cout << "Okay, see you!" << endl;
-                    }
-                }
-                return 0;
-			}
-			else
-			{
-				if (PasswordField[0] == '*')
-				{
-					cout << "NO NO NO NO NO\n";
-				}
-				else if (PasswordField == "your password")
-				{
-					cout << "Very funny.\n";
-				}
-				else
-				{
-					cout << "That's not obfuscated enough.\n";
-				}
-			}
-
+			Username = argv[1];
 		}
 		else
 		{
-			printf("You need to log in first!\n");
+			#ifdef _WIN32
+			Username = "mike-rosoft"
+			#else
+			Username = getlogin();
+			#endif
 		}
-
+		std::string PasswordField;
+		cout << "Logging into Something++ as " << Username << endl;
+		cout << "Please enter your password: ";
+		getline(cin, PasswordField);
+		if (PasswordField == CorrectPassword)
+		{
+			cout << "Welcome, " << Username << "! Creating key file..." << endl;
+			string KeyFileName = Username + ".keyfile";
+			ofstream KeyFile (KeyFileName);
+			cout << "Enter a command!\n";
+			string Command = "";
+			int LoginFun = 0;
+			while (Command != "bye")
+			{
+				cout << Username << " > ";
+				getline(cin, Command);
+				if (Command == "cd")
+				{
+					cout << "There is no CD in the computer.\n" <<
+					"Not only that, but there are no CD drivers in this program.\n" << endl;
+				} 
+				else if (Command == "ls")
+				{
+					cout << "There are no lemons to salvage.\n" <<
+					"Please install a basket in your computer's hard drive and fill it with lemons.\n" << endl;
+				} 
+				else if (Command == "dir")
+				{
+					cout << "Unable to drive in the road.\n" <<
+					"The driver CAR.DLL was not found.\n" << endl;
+				} 
+				else if (Command == "login")
+				{
+					LoginFun++;
+					switch (LoginFun)
+					{
+						case 1:
+							cout << "in\n" << endl;
+							break;
+						case 2:
+							cout << "What? I already logged in.\n" << endl;
+							break;
+						case 3:
+							cout << "As in... I literally logged 'in' to the console.\n" << 
+							"What did you expect?\n" << endl;
+							break;
+						default:
+							cout << "in\n" << endl;
+							break;
+					}
+				} 
+				else if (Command == "bye")
+				{
+					cout << "Okay, see you!" << endl;
+				}
+				else if (Command == "help")
+				{
+					string HelpVar;
+					cout << "Welcome to help. Press Enter to continue.";
+					getline(cin, HelpVar);
+					cout << "\nThank you for using help. Press Enter to exit.";
+					getline(cin, HelpVar);
+					cout << endl;
+				}
+				else if (Command == "run")
+				{
+					cout << "There is nowhere you can run.\n" << endl;
+				}
+				else
+				{
+					cout << Command << " doesn't exist. Type it again, maybe something'll happen!" << endl;
+				}
+			}
+			return 0;
+		}
+		else
+		{
+			if (PasswordField[0] == '*')
+			{
+				cout << "NO NO NO NO NO\n";
+			}
+			else if (PasswordField == "your password")
+			{
+				cout << "Very funny.\n";
+			}
+			else
+			{
+				cout << "That's not obfuscated enough.\n";
+			}
+		}
 	}
 	return 0;
 }
